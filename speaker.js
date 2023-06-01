c655eba91ce430215bd1a42148df4bcc8aed693b
“sadaf <sadaf.batekzai54321@gmial.com>
2023-06-01 23:32:59 +0430

    Fix linters errors

--- speaker.js
+++ speaker.js
@@ -2,38 +2,38 @@ const speakersDataInfo = [
   // create array for speakers part to add the detials instead of adding in html
   {
     Name: 'SohYeong Noh',
-    speakerAffiliation: 'Director of Art Centre Nabi and a board member of CC Korea',
-    speakerInfo: 'Emad Aghajani is a postdoctoral researcher at the SEART research group, at Software Institute, USI, Switzerland.',
+    speakerPosition: 'Director of Art Centre Nabi and a board member of CC Korea',
+    speakerBio: 'Emad Aghajani is a postdoctoral researcher at the SEART research group, at Software Institute, USI, Switzerland.',
     Image: 'image/speaker1.png',
   },
   {
     Name: 'Yochai Benkler',
-    speakerAffiliation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    speakerInfo: 'Juliana is a Lecturer (Assistant Professor) in Software Engineering at the School of Computing Science, University of Glasgow, United Kingdom.',
-    Image: 'image/speaker6.png',
+    speakerPosition: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
+    speakerBio: 'Juliana is a Lecturer (Assistant Professor) in Software Engineering at the School of Computing Science, University of Glasgow, United Kingdom.',
+    Image: 'image/speaker2.png',
   },
   {
     Name: 'Lila tretikov',
-    speakerAffiliation: 'Executive Director of the Wikimedia Foundation',
-    speakerInfo: 'Saba Alimadadi is an Assistant Professor in the Department of Computer Science and Engineering at Simon Fraser University.',
+    speakerPosition: 'Executive Director of the Wikimedia Foundation',
+    speakerBio: 'Saba Alimadadi is an Assistant Professor in the Department of Computer Science and Engineering at Simon Fraser University.',
     Image: 'image/speaker3.png',
   },
   {
     Name: 'Kilnam Chon',
-    speakerAffiliation: 'Saarland University',
-    speakerInfo: 'Sven Apel is an associate professor in the School of Electrical Engineering and Computer Science at Washington State University, Pullman.',
+    speakerPosition: 'Saarland University',
+    speakerBio: 'Sven Apel is an associate professor in the School of Electrical Engineering and Computer Science at Washington State University, Pullman.',
     Image: 'image/speaker4.png',
   },
   {
     Name: 'SohYeong Noh',
-    speakerAffiliation: 'Director of Art Centre Nabi and a board member of CC Korea',
-    speakerInfo: 'Emad Aghajani is a postdoctoral researcher at the SEART research group, at Software Institute, USI, Switzerland.',
+    speakerPosition: 'Director of Art Centre Nabi and a board member of CC Korea',
+    speakerBio: 'Emad Aghajani is a postdoctoral researcher at the SEART research group, at Software Institute, USI, Switzerland.',
     Image: 'image/speaker5.png',
   },
   {
     Name: 'SohYeong Noh',
-    speakerAffiliation: 'Director of Art Centre Nabi and a board member of CC Korea',
-    speakerInfo: 'Emad Aghajani is a postdoctoral researcher at the SEART research group, at Software Institute, USI, Switzerland.',
+    speakerPosition: 'Director of Art Centre Nabi and a board member of CC Korea',
+    speakerBio: 'Emad Aghajani is a postdoctoral researcher at the SEART research group, at Software Institute, USI, Switzerland.',
     Image: 'image/speaker6.png',
   },
 ];
@@ -54,15 +54,15 @@ function renderFeatureSpeaker() {
     cardContent += `
                 
               <li class="speaker speaker-${i + 1}">
-                  <div class="bgFrame"><img class="Contributor" src="${featuredSpeakerInf.Image}" alt="Speaker Picture"></div>
-                  <ul class="Contributor-detail">
-                      <li class="Contributor-name">
+                  <div class="bgFrame"><img class="Contri" src="${featuredSpeakerInf.Image}" alt="Speaker Picture"></div>
+                  <ul class="Contri-detail">
+                      <li class="Contri-name">
                           <h3>${featuredSpeakerInf.Name}</h3>
                       </li>
-                      <li class="Contributor-affiliation">
-                          <h4>${featuredSpeakerInf.speakerAffiliation}</h4>
+                      <li class="Contri-affiliation">
+                          <h4>${featuredSpeakerInf.speakerPosition}</h4>
                       </li>
-                      <li class="contributor-description">
+                      <li class="contri-description">
                           <p>${featuredSpeakerInf.speakerInfo}
                           </p>
                       </li>
@@ -78,26 +78,26 @@ function renderFeatureSpeaker() {
 
   const seeMoreBtn = document.querySelector('.seeMoreBtn');
   const seeLessBtn = document.querySelector('.less');
-  const speaker3 = document.querySelector('.speaker3');
-  const speaker4 = document.querySelector('.speaker4');
-  const speaker5 = document.querySelector('.speaker5');
-  const speaker6 = document.querySelector('.speaker6');
+  const speakerthree = document.querySelector('.speaker-3');
+  const speakerfour = document.querySelector('.speaker-4');
+  const speakerfive = document.querySelector('.speaker-5');
+  const speakersix = document.querySelector('.speaker-6');
 
   function seeMore() {
-    seeMoreBtn.setAttribute('class', 'speaker3');
-    speaker3.classList.remove('speaker3');
-    speaker4.classList.remove('speaker4');
-    speaker5.classList.remove('speaker5');
-    speaker6.classList.remove('speaker6');
+    seeMoreBtn.setAttribute('class', 'speaker-3');
+    speakerthree.classList.remove('speaker-3');
+    speakerfour.classList.remove('speaker-4');
+    speakerfive.classList.remove('speaker-5');
+    speakersix.classList.remove('speaker-6');
     seeLessBtn.setAttribute('class', 'seeMoreBtn');
   }
 
   function seeLess() {
     seeLessBtn.setAttribute('class', 'speaker3 less');
-    speaker3.classList.add('speaker3');
-    speaker4.classList.add('speaker4');
-    speaker5.classList.add('speaker5');
-    speaker6.classList.add('speaker6');
+    speakerthree.classList.add('speaker-3');
+    speakerfour.classList.add('speaker-4');
+    speakerfive.classList.add('speaker-5');
+    speakersix.classList.add('speaker-6');
     seeMoreBtn.setAttribute('class', 'seeMoreBtn');
   }
 
